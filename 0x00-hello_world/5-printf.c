9a9576df24662182a9e6fd964e5588f0ee3ce704
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-08 22:55:13 +0100

    Added C program to print text to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+/**
+ * main - THis is a function to print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,\n);
+	return (0);
+}
