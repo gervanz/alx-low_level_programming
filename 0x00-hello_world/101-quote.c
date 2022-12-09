859c6614f325bedc6bda0db9eeaf36cf5bdc47b9
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-09 03:24:19 +0100

    Added C program to print text to standrd error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print a quote
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
+	return (1);
+}
