e089435ef9a9e610cfc3117035672a4b372a2154
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-09 20:30:39 -0800

    Added C program to print all combinations of three digits, the third always greater than the second which is always greater than the first, separated by commas

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print combination of 2 digits,
+ * the second always larger than the first
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((c != b) && (b != a) && (c != a))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a < '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					else
+						continue;
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
