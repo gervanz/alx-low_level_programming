5b6ea392f92e50135b33aa56b47e93b4b111e62b
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-09 21:23:55 -0800

    dified 102-print_comb5.c to add '0'+num to the putchar function so as to convert the integers to their ASCII representation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,28 +12,26 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= 99; a++)
+	for (a = 0; a <= 99; a++)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			if (b != a)
-			{
-				putchar(a / 10);
-				putchar(a % 10);
-				putchar(' ');
-				putchar(b / 10);
-				putchar(b % 10);
+			putchar('0' + (a / 10));
+			putchar('0' + (a % 10));
+			putchar(' ');
+			putchar('0' + (b / 10));
+			putchar('0' + (b % 10));
 
-				if (a < 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				else
-					continue;
+			if (a < 98)
+			{
+				putchar(',');
+				putchar(' ');
 			}
+			else
+				continue;
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
