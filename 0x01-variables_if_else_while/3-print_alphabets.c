508713c2ac85c4c01c68553c753da7957a5319a9
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-09 17:47:34 -0800

    added C program to print alphabets from a to z and from A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Proogram to print out all of the alphabet in lowercase
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+
+	for (chr = 'A'; chr <= 'Z'; chr++)
+		putchar(chr);
+
+	putchar('\n');
+
+	return (0);
+}
