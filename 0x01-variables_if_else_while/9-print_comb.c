a02a4fa1a685c4e9d7f8586d800df73a2d39e7c0
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-09 19:17:26 -0800

    Added C proggram to print all digits from 0 to 9, separated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digits from 0 to 9
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
