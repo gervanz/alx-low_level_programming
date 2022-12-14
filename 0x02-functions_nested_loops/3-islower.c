d7ec510b7b1d35c3b0fdfb6b7ea649875ecd9074
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-14 05:37:00 +0100

    Modified 3-islower.c to betty standard

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- * _islower: Checks for lower case character
+ * _islower - Checks for lower case character
  *
  * @c: Character (parameter)
  *
- * Return - 1 (Sucess), 0 (Failure)
+ * Return: 1 (Sucess), 0 (Failure)
  */
 
 int _islower(int c)
 {
 	char chr;
 	int x = 0;
-	
+
 	for (chr = 'a'; chr <= 'z'; chr++)
 	{
 		if (chr == c)
 			x = 1;
-        }
-	
+	}
+
 	return (x);
 }
