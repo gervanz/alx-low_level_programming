1e22ca6e03ef8a2475e12b0ae0af1223efdfe0d3
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-14 04:09:51 +0100

    Added 8-main.c, modified main.h, added C program wih funtion to print all the hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day; 00:00 to 23:59
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min ++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
