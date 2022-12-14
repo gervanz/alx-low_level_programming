9b5d9cc621fae5f2813eb24a748d829eadbbf8e5
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-14 05:21:32 +0100

    Added 9-main.c, modified main.h, added C program to print multiplications table in tabular form

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints a tabular multiplications table from 0 to 9,
+ * comma separated
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		b = 0;
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(c + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
