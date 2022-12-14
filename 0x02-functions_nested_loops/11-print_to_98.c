6b672a6ea81791400e5efebb195ee1205472b297
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-14 05:00:37 +0100

    Added 11-main.c, modified main.h, added C program with function to print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,15 +25,15 @@ void print_to_98(int n)
 	}
 	else
 	{
-                for (; n >= 98; n--)
-                {
-                        printf("%d", n);
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
 
-                        if (n == 98)
-                                continue;
-                        else
-                                printf(", ");
-                }
-                printf("\n");
-        }
+			if (n == 98)
+				continue;
+			else
+				printf(", ");
+		}
+		printf("\n");
+	}
 }
