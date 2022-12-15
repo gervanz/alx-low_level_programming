7070b238dc0b5f0095937c3e89b83c91fae52b80
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 20:55:41 +0100

    Added C program with function to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ *
+ * @a: Integer for first positional argument
+ * @b: Integer for second positional argument
+ *
+ * Return: Product of arguments
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
