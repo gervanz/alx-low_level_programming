c6b134c9414f5ea304d8ad71bf02cd57cf39ac5d
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 21:24:24 +0100

    Added C program to print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Print a right-angled isoceles triangle,
+ * right-angled to the right
+ *
+ * @size: Length of sides, per #
+ *
+ * Return: Void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = (size -1); j > i; j--)
+				putchar(' ');
+
+			for (j = 0; j <= i; j++)
+				putchar('#');
+
+			putchar('\n');
+		}
+	}
+
+	else
+		_putchar('\n');
+}
