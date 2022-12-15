4abf43cb214b436fcddaec53c39ad6e374306853
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 13:48:43 +0100

    Added 0-main.c t check for 0, and modified positive_or_negative.c to use mains as input

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,12 +9,12 @@
  * Return: Always 0 (Successful)
  */
 
-int positive_or_negative(void)
+void positive_or_negative(int i)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = i; /*rand() - RAND_MAX / 2;*/
 
 	if (n == 0)
 		printf("%d is zero\n", n);
@@ -23,5 +23,5 @@ int positive_or_negative(void)
 	else
 		printf("%d is negative\n", n);
 
-	return (0);
+	return;
 }
