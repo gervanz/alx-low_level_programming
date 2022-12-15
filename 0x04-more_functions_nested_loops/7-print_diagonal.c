3a2da50bb2734edbb2faf47b0b4341e04bce1248
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 21:16:32 +0100

    Added C program to print diagonal line of n back-slashes long

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Print a diagonal line
+ *
+ * @n: Length of line, per backward slash
+ *
+ * Return: Void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
