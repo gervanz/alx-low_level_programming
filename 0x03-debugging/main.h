59ed98b35a3aff08fd7ac915e948dcfce41cfa73
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 04:13:18 +0100

    Added README file, main.h, and C program to debug adn avert infinite loop

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+void positive_or_negative(int i)
+{
+        if (i == 0)
+                printf("%d is zero\n", i);
+        else if (i > 0)
+                printf("%d is positive\n", i);
+        else
+                printf("%d is negative\n", i);
+}
+
+#endif /* MAIN_H */
