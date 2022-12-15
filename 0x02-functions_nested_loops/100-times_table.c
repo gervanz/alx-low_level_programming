c4ab3d10d4272899b386d73a87597778ee336d85
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 04:02:57 +0100

    Added 100-main.c and C program with function to print n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Prints a tabular multiplications table
+ * from 0 to n(>0 or <15), comma separated
+ *
+ * @n: Extent of multipliction table
+ *
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int a, b, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = a * b;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
