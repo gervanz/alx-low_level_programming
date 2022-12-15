0b831f29fad267d5f5c9f2ffa52703a27dd88aff
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 21:22:45 +0100

    Modified 8-print_square to print for size less than or eq to 0

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,18 @@
 
 void print_square(int size)
 {
-	int i, j;
-
-	for (j = 0; j < size; j++)
+	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
-			_putchar('#');
+		int i, j;
 
-		_putchar('\n');
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
