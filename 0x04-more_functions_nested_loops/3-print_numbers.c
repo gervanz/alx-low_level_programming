4519b03637798f49d1296e37b6cd116efd245374
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-15 20:58:32 +0100

    Added C program with function to print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints from 0 to 9
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
