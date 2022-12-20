3b733a92dd7388dc3b336c21ad7a5bd1fcff5cef
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 00:18:39 +0100

    Added 4-main.c and C program to print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s - String array
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	/* Initialise variable to loop string index */
+	int len = 0;
+
+	/* Loop through string array to last index*/
+	while (s[len] != '\0')
+		len++;
+
+	/* Print string from highest index, decreasing index value */
+	for (; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+
+	_putchar('\n');
+}
