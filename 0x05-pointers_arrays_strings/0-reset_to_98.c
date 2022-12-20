465ccca43dee6fdcd4193e8d95d84932eeb194fe
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 00:07:37 +0100

    Added README file, main.h, putchar.c and C program with function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the parameter to 98
+ *
+ * @n: Pointer to int Variable
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A README file for the AlX project: C - Pointers, Arrays and Strings
