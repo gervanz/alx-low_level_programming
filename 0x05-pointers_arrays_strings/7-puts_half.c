6b619a82f177122ab1b68b5b580d4a87bfbd2fa5
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 00:29:44 +0100

    Added 7-main.c and C program with function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - Prints second half of a string,
+ *
+ * @str: String Array
+ *
+ * Return: Void
+ */
+
+void puts_half(char *str)
+{
+	/* Initialise variable for index looping */
+	int len = 0, i;
+
+	/* Loop through the string array till get last index */
+	while (str[len] != '\0')
+		len++;
+
+	/* Print characters from halfway through the string */
+	for (i = len/2; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	/* Print new line */
+	_putchar('\n');
+}
