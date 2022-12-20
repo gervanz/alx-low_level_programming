6c69701fe0668d6811da30081fb7a1bb9a597c05
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 00:34:25 +0100

    Modified 7-puts_half.c to exclude middle character

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,16 @@ void puts_half(char *str)
 	/* Initialise variable for index looping */
 	int len = 0, i;
 
-	/* Loop through the string array till get last index */
+	/* Loop through the string array till get last index
+	 * if length is odd, print half minus middle character
+	 */
 	while (str[len] != '\0')
 		len++;
+	if (len % 2 != 0)
+		len -= 1;
 
 	/* Print characters from halfway through the string */
-	for (i = len/2; i <= len; i++)
+	for (i = len / 2; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
