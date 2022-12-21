dda6ed4f6e4de43d97384add3172bf53d945e32f
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 04:40:17 +0100

    Added 5-main.c and C program with function to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: String parameter
+ *
+ * Return: Void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char clip;
+
+	while (*(s + i) != '\0')
+		i++;
+	i -= 1;
+
+	while (len < i)
+	{
+		clip = s[i];
+		s[i] = s[len];
+		s[len] = clip;
+		len++;
+		i--;
+        }
+}
