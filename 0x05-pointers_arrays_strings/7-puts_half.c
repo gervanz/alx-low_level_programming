e3dfbd2cfe637979ab0f3b7206b9907a4836c665
Tonii <abudu.m1700302@st.futminna.edu.ng>
2022-12-21 04:02:38 +0100

    Modified 7-puts_half.c to remove extra character for odd length strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,19 +14,24 @@ void puts_half(char *str)
 	/* Initialise variable for index looping */
 	int len = 0, i;
 
-	/* Loop through the string array till get last index
-	 * if length is odd, print half minus middle character
-	 */
+	/* Loop through the string array till get last index */
 	while (str[len] != '\0')
 		len++;
-	if (len % 2 != 0)
-		len = len + 1;
 
-	/* Print characters from halfway through the string */
-	for (i = len / 2; i < len; i++)
-	{
-		_putchar(str[i]);
-	}
+	/**
+	 * Check if length is odd, print half minus middle character
+	 * If not, print characters from halfway through the string
+	 */
+	 if (len % 2 == 0)
+		 for (i = len / 2; i < len; i++)
+		 {
+			 _putchar(str[i]);
+		 }
+	 else
+		 for (i = (len + 1) / 2; i < len; i++)
+                 {
+                         _putchar(str[i]);
+                 }
 
 	/* Print new line */
 	_putchar('\n');
