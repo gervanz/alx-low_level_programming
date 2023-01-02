a94618fec546e25de162599f639ffc9aac935720
Tonii <abudu.m1700302@st.futminna.edu.ng.com>
2023-01-02 19:48:43 +0100

    Modified 100-rot-13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,7 +4,7 @@
 /**
  * rot13 -  Encodes a string using rot13.
  *
- * @s: String to encode with ROT13
+ * @str: String to encode with ROT13
  *
  * Return: String encoded in ROT13
  */
@@ -16,14 +16,16 @@ char *rot13(char *str)
 
 	while (enc[i] != '\0')
 	{
-		if ((enc[i] >= 'a' && enc[i] <= 'm') || (enc[i] >= 'A' && enc[i] <= 'M'))
+		if ((enc[i] >= 'a' && enc[i] <= 'm') ||
+				(enc[i] >= 'A' && enc[i] <= 'M'))
 			enc[i] += 13;
-		else if ((enc[i] >= 'n' && enc[i] <= 'z') || (enc[i] >= 'N' && enc[i] <= 'Z'))
+		else if ((enc[i] >= 'n' && enc[i] <= 'z') ||
+				(enc[i] >= 'N' && enc[i] <= 'Z'))
 			enc[i] -= 13;
 
 		i++;
 	}
-	
+
 	i++;
 
 	return (enc);
