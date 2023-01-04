e7df3ddcbee05c879bcca3f5d74a4fdc6942fc1d
Tonii <abudu.m1700302@st.futminna.edu.ng.com>
2023-01-05 00:14:24 +0100

    Added README file, main.h, _putchar.c, 0-main.c and C program with recursive function to print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ *
+ * @s: Input string
+ *
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
