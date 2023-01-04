ba25646b21e034ccd2716347111f66d4f2806bf4
Tonii <abudu.m1700302@st.futminna.edu.ng.com>
2023-01-05 00:28:28 +0100

    Added 2-main.c and C program with recursive function to retrn the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ *
+ * @s: Input string
+ *
+ * Return: Length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	
+	return (0);
+}
