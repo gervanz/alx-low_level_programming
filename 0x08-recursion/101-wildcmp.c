3311927691d457080905e1a26c7b8c1458836c4c
Tonii <abudu.m1700302@st.futminna.edu.ng.com>
2023-01-06 05:26:22 +0100

    Modified 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,9 +17,9 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if ((*s2 == '*' && *(s1 + 1) && *s2) || (*s2 == '*' && *(s2 + 1) && *s1))
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s2 == '*' && *(s2 + 1) && *s1)
 		return (0);
 
 	return (0);
