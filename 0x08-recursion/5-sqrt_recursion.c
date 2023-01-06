24fc1918076be5eb30c247edc75e131557309dc9
Tonii <abudu.m1700302@st.futminna.edu.ng.com>
2023-01-06 02:13:24 +0100

    Modified 5-sqrt_recursion.c to return correct output for 1 and 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,7 +15,8 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-
+	if ((n == 0) || (n == 1))
+		return (n);
 	return (_sqrt(i, n));
 }
 
@@ -34,6 +35,5 @@ int _sqrt(int i, int n)
 		return (-1);
 	else if (i * i == n)
 		return (i);
-
 	return (_sqrt(i + 1, n));
 }
